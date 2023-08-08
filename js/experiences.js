728bbd3cd483381040ffbea2f1edf31fa58571a4
William Pasquier <wpasquier61@gmail.com>
2023-08-08 12:27:38 +0200

    Create skill container
    Add all logos
    Create json
    Create container with json file

--- js/experiences.js
+++ js/experiences.js
@@ -2,6 +2,7 @@
 
 var experienceEvents = () => {
     tabsOnClick();
+    getSkillsData();
 }
 
 var tabsOnClick = () => {
@@ -21,5 +22,43 @@ var tabsOnClick = () => {
             sections.forEach( section => { section.classList.remove('active-section') });
             sections[index].classList.add('active-section')
         })
+    })
+}
+
+function getSkillsData() {
+    readJSON('/resources/json/skills.json', createSkills)
+}
+
+function createSkills(skillsData) {
+    const skillsContainer = document.querySelector('.skills-content');
+
+    if (skillsData.length == 0) {
+        skillsContainer.innerHTML = `<span class="none-text">No skills found</span>`;
+        return;
+    }
+
+    skillsData.forEach(skill => {
+        let skillList = '';
+
+        skill.skills.forEach(s => {
+            skillList += `
+                <li class="skill">
+                    <img src="./resources/images/icons/skills/${skill.type}/${s.logo}" alt="" class="skill-logo">
+                    <p class="skill-name">${s.name}</span></p>
+                    <div class="rate-background">
+                        <div class="rate-bar" style="width: ${s.rate}%;"></div>
+                    </div>
+                </li>
+            `;
+        });
+
+        skillsContainer.innerHTML += `
+            <div class="skills-container">
+                <h4 class="skills-title">${skill.type}</h4>
+                <ul class="skills-list">
+                    ${skillList}
+                </ul>
+            </div>
+        `;
     })
 }
