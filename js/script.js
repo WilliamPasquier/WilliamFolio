0dbe8eb2916fda93b040e76019c1a202e3a27e30
William Pasquier <wpasquier61@gmail.com>
2023-11-08 12:28:57 +0100

    Fix : Remove useless comment and value

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ window.onload = () => {
     whoEvents();
     projectsEvents();
     experienceEvents();
-
-    entryAnimation();
 }
 
 window.onresize = () => {
@@ -17,19 +15,3 @@ window.onresize = () => {
 window.onscroll = () => {
     changeNavbarOnScroll();
 }
-
-var entryAnimation = () => {
-    const elements = document.querySelectorAll('.entry_anim');
-    let index = 0;
-
-    const interval = setInterval(() => {
-        if (index < elements.length) {
-            const el = elements[index];
-            el.style.animation = '2s anim-lineUp ease-out';
-            el.style.opacity = '1';
-            index++;
-        } else {
-            clearInterval(interval);
-        }
-    }, 400);
-};
