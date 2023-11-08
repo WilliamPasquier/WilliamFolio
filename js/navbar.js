183ee1e337ad79267f2a30be7d201e6c22913012
William Pasquier <wpasquier61@gmail.com>
2023-11-08 11:19:19 +0100

    [Fix] Update responsive navbar

--- js/navbar.js
+++ js/navbar.js
@@ -2,6 +2,8 @@
 
 var navEvents = () => {
     burgerBtnDetection();
+    // TODO : Hide navbar when scroll down
+    // hideNavbarOnScroll();
 }
 
 var burgerBtnDetection = () => {
@@ -24,7 +26,7 @@ var burgerBtnDetection = () => {
 var changeNavbarOnScroll = () => {
     const navbar = document.querySelector('.primary-header');
 
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) { 
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) { 
         navbar.style.backgroundColor = 'rgba( 27, 22, 49, 0.9 )';
         navbar.style.boxShadow = '0 0.5rem 2rem 0 rgba( 0, 0, 0, 0.6 )';
 
@@ -32,8 +34,25 @@ var changeNavbarOnScroll = () => {
             navbar.style.backdropFilter = 'blur(6px)'
         }
     } else {
-        navbar.style.backgroundColor = 'transparent';
         navbar.style.backdropFilter = 'none';
         navbar.style.boxShadow = 'none';
     }
 }
+
+// TODO : Hide navbar when scroll down
+// var hideNavbarOnScroll = () => {
+//     const navbar = document.querySelector('.primary-header');
+//     var prevScrollpos = window.scrollY;
+
+//     window.onscroll = function() {
+//         var currentScrollPos = window.scrollY;
+        
+//         if (prevScrollpos < currentScrollPos) {
+//             navbar.style.top = "-100%";
+//         } else {
+//             navbar.style.top = "0";
+//         }
+
+//         prevScrollpos = currentScrollPos;
+//     }
+// }
